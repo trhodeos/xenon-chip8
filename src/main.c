6f3a284594a668af1bdc5d4054319e524ce55856
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2011-09-12 00:16:39 -0500

    Hello world!

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <xenos/xenos.h>
+#include <console/console.h>
+
+int main(void) {
+  console_init();
+  printf("Hello, world!\n");
+  return 0;
+}
