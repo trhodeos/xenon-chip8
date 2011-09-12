28267b3d37f564788b209d3a16c5e584ce1a5bb5
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2011-09-12 01:34:03 -0500

    Add basic type definitions

---
+++ src/types.h
@@ -0,0 +1,12 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+// general
+typedef unsigned short ushort;
+typedef unsigned char uchar;
+
+// chip-8 specific
+typedef ushort opcode;
+typedef uchar byte;
+
+#endif // TYPES_H
